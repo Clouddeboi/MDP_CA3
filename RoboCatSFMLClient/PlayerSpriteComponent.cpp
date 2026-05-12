1d08df85813850692a92677fedf9d152f04ef92e
Michal Becmer <michalcloud2003@gmail.com>
2026-05-12 18:36:59 +0100

    🐛 Fixed player scaling issue
    The player collider would scale but not the sprite

--- RoboCatSFMLClient/PlayerSpriteComponent.cpp
+++ RoboCatSFMLClient/PlayerSpriteComponent.cpp
@@ -9,8 +9,12 @@ sf::Sprite& PlayerSpriteComponent::GetSprite()
 	// Update the sprite based on the game object stuff.
 	auto pos = mGameObject->GetLocation();
 	auto rot = mGameObject->GetRotation();
+	float scale = mGameObject->GetScale();
+
 	m_sprite.setPosition(pos.mX, pos.mY);
 	m_sprite.setRotation(rot);
+	m_sprite.setScale(scale, scale);
+
 	RoboCat* player = dynamic_cast<RoboCat*>(mGameObject);
 	Vector3 playerColor = player->GetColor();
 	m_sprite.setColor(sf::Color(playerColor.mX, playerColor.mY, playerColor.mZ, 255));

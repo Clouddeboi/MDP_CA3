b506e4fb1f0d2b0d3df32c13d55f9a84d62d81d9
Michal Becmer <michalcloud2003@gmail.com>
2026-05-12 02:51:51 +0100

    🍱 Replaced player sprite

--- RoboCatSFMLClient/SpriteComponent.cpp
+++ RoboCatSFMLClient/SpriteComponent.cpp
@@ -4,13 +4,11 @@
 SpriteComponent::SpriteComponent(GameObject* inGameObject) :
 	mGameObject(inGameObject)
 {
-	//and add yourself to the rendermanager...
 	RenderManager::sInstance->AddComponent(this);
 }
 
 SpriteComponent::~SpriteComponent()
 {
-	//don't render me, I'm dead!
 	RenderManager::sInstance->RemoveComponent(this);
 }
 
@@ -19,16 +17,17 @@ void SpriteComponent::SetTexture(TexturePtr inTexture)
 	auto tSize = inTexture->getSize();
 	m_sprite.setTexture(*inTexture);
 	m_sprite.setOrigin(tSize.x / 2, tSize.y / 2);
-	m_sprite.setScale(sf::Vector2f(1.f * mGameObject->GetScale(), 1.f * mGameObject->GetScale()));
 }
 
 sf::Sprite& SpriteComponent::GetSprite()
 {
-	// Update the sprite based on the game object stuff.
 	auto pos = mGameObject->GetLocation();
 	auto rot = mGameObject->GetRotation();
+	float scale = mGameObject->GetScale();
+
 	m_sprite.setPosition(pos.mX, pos.mY);
 	m_sprite.setRotation(rot);
+	m_sprite.setScale(scale, scale);
 
 	return m_sprite;
 }
